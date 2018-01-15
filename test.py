a2ae0f5569d57a18a2983c99b4f2cabff08f83b8
H3lloW0rld <Nguyn Trng Khnh>
2018-01-15 23:44:50 +0700

    add module pylibelf

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from elftools.elf.elffile import ELFFile
 
+import os
+
+print os.getcwd()
+
 with open('uaf32', 'rb') as file:
     e = ELFFile(file)
-    header = e.header
-    print header
+    print e.get_section_by_name('.symtab').get_symbol_by_name('main')
