539adcd52a6465b865f8545ee0be20928ec61371
H3lloW0rld <Nguyn Trng Khnh>
2018-01-25 00:49:11 +0700

    use pyelftools instead of pylibelf

--- test.py
+++ test.py
@@ -2,36 +2,95 @@ import sys
 import os
 import re
 
-from pylibelf import pylibelf as libelf
-from pylibelf import elfconstants, elfutils
+from elftools.elf.elffile import ELFFile
+from elftools.common.exceptions import ELFError
+from elftools.elf.sections import SymbolTableSection
+from capstone import *
 
 COMMAND_LIST = [
-    r"^(disass) ([\w\d_]+)$" # disassemble a function
+    r"^(disass) ([\w\d_]+)$", # disassemble a function
+    r"(quit)", # exit
 ]
-
-def getSymbolTable(elf):
-    symtabSection = elf.getSectionByName(elfconstants.ELF_SECTION_TYPES['SHT_SYMTAB'])
-    return libelf.Elf_SymbolTable.parse(elfutils.ReadData(symtabSection.sectionRawData), elf.getType().value)
 
 def printHelp():
     print "-h hihi"
 
-def getParameter(command):
+def get_parameter(command):
     for pattern in COMMAND_LIST:
-        m = re.match(command, pattern)
+        m = re.match(pattern, command)
         if (m is not None):
             return m.groups()
     return None
 
 def handle(filePath):
-    elf = libelf.ELF(filePath)
-    arch = elf.elfHdr.e_machine
-    
-    if (arch == elfconstants.ELF_MACHINE_TYPES[])
-    print "Welcome to BasicDisass, please type 'help' to get more information!"
+    file = open(filePath, "rb")
+    try:
+        elf = ELFFile(file)
+        arch = elf.get_machine_arch()
+        
+        # determine architecture
+        if arch == 'x86':
+            cs_arch = CS_ARCH_X86
+            cs_mode = CS_MODE_32
+        elif arch == 'x64':
+            cs_arch = CS_ARCH_X86
+            cs_mode = CS_MODE_64
+        elif arch == 'ARM':
+            cs_arch = CS_ARCH_ARM
+            cs_mode = CS_MODE_ARM
+        elif arch == 'AArch64':
+            cs_arch = CS_ARCH_ARM64
+            cs_mode = CS_MODE_ARM
+        else:
+            sys.stderr.write("Don't support ELF format machine\n")
+            file.close()
+            sys.exit(1)
+
+        # get .text section
+        code_section = elf.get_section_by_name('.text')
+        # get .text section's data
+        code = code_section.data()
+        # get .text section virtual address
+        addr = code_section['sh_addr']
+        # get symbol table from .symtab section or .dynsym section
+        symbol_tables = [s for s in elf.iter_sections() if isinstance(s, SymbolTableSection)]
+    except ELFError as ex:
+        sys.stderr.write("ELF error: %s\n" % ex)
+        file.close()
+        sys.exit(1)
+
+    # initialize Capstone engine
+    md = Cs(cs_arch, cs_mode)
 
     while True:
+        flag = False
         command = raw_input(">> ")
+        param = get_parameter(command)
+
+        if (param is None):
+            print "Invalid command!"
+        elif (param[0] == "disass"):
+            # disassemble function
+            for section in symbol_tables:
+                for s in section.iter_symbols():
+                    # find the given symbol in symbol table
+                    if s.name == param[1]:
+                        flag = True
+                        symbol = s
+                        break
+                if flag:
+                    break
+            if flag:
+                sp = symbol['st_value'] - addr
+                for (address, size, mnemonic, op_str) in md.disasm_lite(code[sp: sp + symbol['st_size'] + 1], symbol['st_value']):
+                    print("0x%x:\t%s\t%s" % (address, mnemonic, op_str))
+            else:
+                print "Can't not find symbol!"
+        elif (param[0] == "quit"):
+            break
+
+    file.close()
+    
 
 def main():
     if len(sys.argv) == 2:
@@ -41,3 +100,6 @@ def main():
             print "File doesn't exist!"
     else:
         printHelp()
+
+
+main()
